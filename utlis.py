fix rotation

utlis.py
@@ -4,12 +4,11 @@ import collections
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 
-%matplotlib inline
-
 from __future__ import division
 
 import torch 
 import random
+import math
 
 import numpy as np
 import cv2
@@ -91,43 +90,16 @@ def adjust_box(poly):
     adjusted_bbox = (min(x), min(y), max(x) - min(x), max(y) - min(y))
     return key_points, adjusted_bbox
 
-def rbbox_transform(ex_rois, gt_rois):
-
+def rbbox_transform(poly, angle):
+    pi = math.pi()
+    rad = angle / 360 * 2 * pi
+    rot_mat = np.array([[np.cos(rad), -np.sin(rad)], [ np.sin(rad), np.cos(rad)]])
     
-    """
-    implement rotating bounding box
-    """
-	ex_widths = ex_rois[:, 3] 
-	ex_heights = ex_rois[:, 2] 
-	ex_ctr_x = ex_rois[:, 0]
-	ex_ctr_y = ex_rois[:, 1]
-	ex_angle = ex_rois[:, 4] 	
-
-	gt_widths = gt_rois[:, 3]
-	gt_heights = gt_rois[:, 2]
-	gt_ctr_x = gt_rois[:, 0]
-	gt_ctr_y = gt_rois[:, 1]
-	gt_angle = gt_rois[:, 4]
-
-	targets_dx = (gt_ctr_x - ex_ctr_x)*1.0 / ex_widths
-    targets_dy = (gt_ctr_y - ex_ctr_y)*1.0 / ex_heights
-    targets_dw = np.log(gt_widths*1.0 / ex_widths)
-    targets_dh = np.log(gt_heights*1.0 / ex_heights)
-
-	targets_da = gt_angle - ex_angle
-
-	targets_da[np.where((gt_angle<=-30) & (ex_angle>=120))]+=180
-	targets_da[np.where((gt_angle>=120) & (ex_angle<=-30))]-=180
-
-	targets_da = 3.14159265358979323846264338327950288/180*targets_da
-	
-
-	targets = np.vstack(
-		 (targets_dx, targets_dy, targets_dw, targets_dh, targets_da)
-	).transpose()
-
-	return targets
-
+    ctr = np.mean(poly,axis=0)
+    poly_ctr = poly - ctr
+    poly_rot = np.dot(rot_mat, poly_ctr) + ctr
+    
+    return poly_rot
 
 
 # Multi-scale usage